39e89df2b94aaa6aa7fd3d9c64cb9a19131478c8
Vitaliy336 <Vitaliyshevchyk336@gmail.com>
2017-05-10 13:57:48 +0300

    add menu and some alpha things

---
+++ app/src/main/java/com/example/vitaliy/mazeproject/MyService.java
@@ -0,0 +1,40 @@
+package com.example.vitaliy.mazeproject;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+/**
+ * Created by vitaliy on 5/10/17.
+ */
+public class MyService extends Service {
+    private static final String TAG = "MyService";
+    MediaPlayer player;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, "My Service Created", Toast.LENGTH_LONG).show();
+
+        player = MediaPlayer.create(this, R.raw.frankumvintageelecropoploop);
+        player.setLooping(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "My Service Stopped", Toast.LENGTH_LONG).show();
+        player.stop();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startid) {
+        Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
+        player.start();
+    }
+}
